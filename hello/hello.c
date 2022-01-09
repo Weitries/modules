e0bba51d2664d95aeae77595f239a7832bde6c05
Weitries <lianghuic_t@163.com>
2022-01-09 06:50:13 -0800

    modified hello module: 	print current thread'name and pid.

--- hello/hello.c
+++ hello/hello.c
@@ -1,16 +1,19 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/sched.h>
 
 static int hello_init(void)
 {
-    printk("Hello Weitries!\n");
-    printk("Nice to meet you!\n");
+    printk(KERN_INFO "Hello Weitries!\n");
+    printk(KERN_INFO "Nice to meet you!\n");
+    printk(KERN_INFO "This process is \"%s\" (pid %i) \n", current->comm, current->pid);
     return 0;
 }
 
 static void hello_exit(void)
 {
-    printk("Goodbye! Weitries!\n");
+    printk(KERN_INFO "This process is \"%s\" (pid %i) \n", current->comm, current->pid);
+    printk(KERN_INFO "Goodbye! Weitries!\n");
     return;
 }
 
